2cc93784c776a1018d7c00d9a03ff2a2dc9be962
Kauã Oliveira Seixas <146961155+Kaua281@users.noreply.github.com>
2026-09-20 15:16:30 +0000

    Adiciona configuração do devcontainer para rodar Java e classe Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,26 @@
+public class Calculadora {
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divisão por zero");
+        }
+
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiplicar(2, 2));
+    }
+}
